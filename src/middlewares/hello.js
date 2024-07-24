5b5e150a6d4463353be8455bc374387340526dc1
if.ltd <>
2024-07-24 10:58:26 +0800

    t21

--- src/middlewares/hello.js
+++ src/middlewares/hello.js
@@ -1,7 +1,7 @@
 
 module.exports = (config, { strapi }) => {
     return (context, next) => {
-        console.log('context', context);
+        // console.log('context', context);
         if (context.request.url === '/') {
             // react redircet to /admin
             context.response.redirect('/admin');
