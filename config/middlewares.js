cf08d5de73d40b71330da557a2b4db137544dc2f
if.ltd <>
2024-07-25 16:06:48 +0800

    dev-15

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,32 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            env('CDN_URL').replace('https://', 'http://'),
+            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            env('CDN_URL').replace('https://', 'http://'),
+            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
